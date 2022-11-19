cef88ae8e7fdbcb281c8b75254160e632ca1cc0c
VIET-NGUYEN\Administrator <quocviet.ce@gmail.com>
2022-11-19 13:30:13 +0700

    add file .env

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import { QRPay } from './qr-pay';
 
 const app: Application = express();
 
+require('dotenv/config');
+
 // Middleware
 app.use(bodyParser.json());
 
@@ -29,5 +31,7 @@ app.post(
   },
 );
 
-
-app.listen(PORT, () => console.log('Server running'));
+// app.listen(PORT, () => console.log('Server running'));
+app.listen(process.env.PORT || 5000, () => {
+  console.log('Server is running ', PORT);
+});
