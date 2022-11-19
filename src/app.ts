18b0ade4cd17867d7fe918892ea016974b62504b
VIET-NGUYEN\Administrator <quocviet.ce@gmail.com>
2022-11-19 13:37:14 +0700

    Update port

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ require('dotenv/config');
 // Middleware
 app.use(bodyParser.json());
 
-const PORT = 5000;
+// const PORT = 5000;
 
 app.post(
   '/decode-vietqr',
@@ -32,6 +32,9 @@ app.post(
 );
 
 // app.listen(PORT, () => console.log('Server running'));
-app.listen(process.env.PORT || 5000, () => {
+
+const PORT = process.env.port || '5000';
+
+app.listen(PORT, () => {
   console.log('Server is running ', PORT);
 });
