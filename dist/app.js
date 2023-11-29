82bd268c9ce90cda0736607d605f1440352927a8
Viet Nguyen <>
2023-11-29 21:19:56 +0700

    app.ts

--- dist/app.js
+++ dist/app.js
@@ -1,14 +1,13 @@
-'use strict';
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
-Object.defineProperty(exports, '__esModule', { value: true });
-const express_1 = __importDefault(require('express'));
-const body_parser_1 = __importDefault(require('body-parser'));
-const cors_1 = __importDefault(require('cors'));
-const morgan_1 = __importDefault(require('morgan'));
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const cors_1 = __importDefault(require("cors"));
+const morgan_1 = __importDefault(require("morgan"));
+const qr_pay_1 = require("./src/qr-pay");
 // Routes
 const qrCodeRoutes = require('./src/routers/qrcode');
 const app = (0, express_1.default)();
@@ -19,28 +18,28 @@ app.options('*', (0, cors_1.default)());
 app.use(body_parser_1.default.json());
 app.use((0, morgan_1.default)('tiny'));
 const api = process.env.API_URL;
-app.post(
-  '/decode-vietqr',
-  (req, res, next) => {
+app.post('/decode-vietqr', (req, res, next) => {
     try {
-      const content_qr_code = req.body.content;
-      const qrPay = new QRPay(content_qr_code);
-      if (qrPay.isValid) {
-        return res.status(200).json({ success: true, data: qrPay });
-      } else {
-        return res
-          .status(401)
-          .json({ success: false, error: 'QR Code Invalid' });
-      }
-    } catch (error) {
-      return res.status(400).json({ success: false, error: 'QR Code Invalid' });
+        const content_qr_code = req.body.content;
+        const qrPay = new qr_pay_1.QRPay(content_qr_code);
+        if (qrPay.isValid) {
+            return res.status(200).json({ success: true, data: qrPay });
+        }
+        else {
+            return res
+                .status(401)
+                .json({ success: false, error: 'QR Code Invalid' });
+        }
     }
-  },
-);
+    catch (error) {
+        return res.status(400).json({ success: false, error: 'QR Code Invalid' });
+    }
+});
 // Routers
-app.use(`${api}/decode`, qrCodeRoutes);
+// app.use(`${api}/decode`, qrCodeRoutes);
 // app.listen(PORT, () => console.log('Server running'));
 const PORT = process.env.port || 6000;
 app.listen(PORT, () => {
-  console.log('Server is running ', PORT);
+    // console.log('api: ', api);
+    console.log('Server is running ', PORT);
 });
